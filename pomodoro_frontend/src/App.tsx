7f8750f4dd367af7b14dc8921768c34c01f7a814
Emiliano Lopez <emiliano.ll289@gmail.com>
2025-10-14 17:52:17 -0600

    feat: Integrate react router and configure core pages
    - Setup navigation between main app sections

--- pomodoro_frontend/src/App.tsx
+++ pomodoro_frontend/src/App.tsx
@@ -1,11 +1,20 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Timer from "./pages/Timer";
+import Dashboard from "./pages/Dashboard";
+import Tags from "./pages/Tags";
+import History from "./pages/History";
 
 function App() {
   return (
-    <>
-      <Timer />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/timer" replace />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/tags" element={<Tags />} />
+        <Route path="history" element={<History />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
